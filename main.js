46fb652f4e336361cbd308780704be82c153ace1
Faiza Muskan <108839239+faiza-muskan@users.noreply.github.com>
2022-11-09 15:19:04 +0000

    a simple js-clock
    practicing js with simple projects.

---
+++ main.js
@@ -0,0 +1,18 @@
+const hours = document.getElementById("hr");
+const minutes = document.getElementById("min");
+const seconds = document.getElementById("sec");
+
+setInterval(() => {
+  let date = new Date();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
+
+  let hr = hour * 30;
+  let min = minute * 6;
+  let sec = second * 6;
+
+  hours.style.transform = `rotateZ(${hr}deg)`;
+  minutes.style.transform = `rotateZ(${min}deg)`;
+  seconds.style.transform = `rotateZ(${sec}deg)`;
+}, 1000);
